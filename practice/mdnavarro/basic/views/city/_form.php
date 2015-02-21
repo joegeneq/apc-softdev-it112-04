cfc560fb95e46b7c81fe49452f5de76d303b2b55
mdnavarro <markkevinnavarro@gmail.com>
2015-02-21 09:16:43 -0800

    Successfully inserted and implemented dropDownList to the practice/basic

--- practice/mdnavarro/basic/views/city/_form.php
+++ practice/mdnavarro/basic/views/city/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\Province;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\City */
@@ -12,13 +14,19 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id')->textInput() ?>
+    <?= $form->field($model, 'city_code')->textInput(['maxlength' => 32]) ?>
 
-    <?= $form->field($model, 'city_code')->textInput(['maxlength' => 45]) ?>
+    <?= $form->field($model, 'city_description')->textInput(['maxlength' => 32]) ?>
+        
+        <?php 
+                $province=province::find()->all();
 
-    <?= $form->field($model, 'city_description')->textInput(['maxlength' => 45]) ?>
+                $listData=ArrayHelper::map($province,'id','province_code');
 
-    <?= $form->field($model, 'province_id')->textInput() ?>
+                echo $form->field($model, 'province_id')->dropDownList(
+                                                                $listData, 
+                                                                ['prompt'=>'Select province...']);
+        ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
--- practice/mdnavarro/basic/views/province/_form.php
+++ practice/mdnavarro/basic/views/province/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\Region;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Province */
@@ -11,15 +13,21 @@ use yii\widgets\ActiveForm;
 <div class="province-form">
 
     <?php $form = ActiveForm::begin(); ?>
+        
+    <?= $form->field($model, 'province_code')->textInput(['maxlength' => 32]) ?>
 
-    <?= $form->field($model, 'id')->textInput() ?>
+    <?= $form->field($model, 'province_description')->textInput(['maxlength' => 32]) ?>
 
-    <?= $form->field($model, 'province_code')->textInput(['maxlength' => 45]) ?>
+        <?php 
+                $region=region::find()->all();
 
-    <?= $form->field($model, 'province_description')->textInput(['maxlength' => 45]) ?>
+                $listData=ArrayHelper::map($region,'id','region_code');
 
-    <?= $form->field($model, 'region_id')->textInput() ?>
-
+                echo $form->field($model, 'region_id')->dropDownList(
+                                                                $listData, 
+                                                                ['prompt'=>'Select region...']);
+        ?>
+        
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
