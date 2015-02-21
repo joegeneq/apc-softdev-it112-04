aa39235bce9df248c8c4814ad8f661afe7ae419f
clronario <kerstenten@gmail.com>
2015-02-21 14:31:40 -0800

    pulled updated copy of repository

--- practice/clronario/basic2/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=IT112_exercise1',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
