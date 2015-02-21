ed2fa3211f325669bf44ac3a603155e8f2bf10f1
lmcalanno <florcalanno@gmail.com>
2015-02-21 15:21:51 +0800

    update it112_exercise1

--- practice/lmcalanno/basic/config/db.php
+++ practice/lmcalanno/basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=it112_exercise1',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
