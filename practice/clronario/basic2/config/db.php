1498ef35ec6c7c8fb38572dce39511af654f4b89
clronario <kerstenten@gmail.com>
2015-02-21 08:39:11 +0800

    updated db.php and web.php for exercise1

--- practice/clronario/basic2/config/db.php
+++ practice/clronario/basic2/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=IT112_exercise1',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
