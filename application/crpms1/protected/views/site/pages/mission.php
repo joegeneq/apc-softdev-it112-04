b131039deb821b76217600fd00cb8ad8045bef0b
mdnavarro <markkevinnavarro@gmail.com>
2015-02-27 15:53:26 -0800

    re-commit: inserted dropDownList Menu for about page

---
+++ application/crpms1/protected/views/site/pages/mission.php
@@ -0,0 +1,25 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - Mission';
+$this->breadcrumbs=array(
+	'Mission',
+);
+?>
+<center><img width="200" height="240" src=""></center><br>
+
+<p><center><h3>Mission Statement</h3></center></p>
+
+<p><li><h4>SSMC regards all those it serve with the respect,kindness and compassion;</h4></li></p><br>
+
+<p><li><h4>SSMC promotes health and wellness pratices in the worplaces of its client as well as of itself for sound business outcomes;</h4></li></p><br>
+
+<p><li><h4>SSMC continiously strives to have a full assortment of suitable equipment and facilities to etter serve its patients;</h4></li></p><br>
+
+<p><li><h4>SSMC promotes the development of a consortium of competent and effective health professionals and workers who are recognized as part of the healthcare family worthhy of being treated with loyalthy, respect and dignity;</h4></li></p><br>
+
+<p><li><h4>SSMC networks with labor intensive institutions and acts with outmost, honesty ,integrity and fairness to the best interest of the working masses;</h4></li></p><br>
+
+<p><li><h4>SSMC maintains a sound Quality Assurance Program, continiously strives for improvement and supports its efforts with certification from various recognized accrediting bodies in Quality, Environment, Safety and health and other areas that may e deemed relevant and beneficial for all concerned at any given time; and,</h4></li></p><br>
+
+<p><li><h4> SSMC networks with benevolent individuals and intstitutions to ensure that resources are made available to effectively and advocacy programs.</h4></li></p><br>
+
+
---
+++ application/crpms1/protected/views/site/pages/vision.php
@@ -0,0 +1,16 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - Vision';
+$this->breadcrumbs=array(
+	'Vision',
+);
+?>
+<center><img width="200" height="240" src=""></center><br>
+
+<p><center><h3>Vision</h3></center></p><br>
+<p><li><h4>SSMC is one of the country's leading, trusted and affordable one stop provider of health care solutions for employers, their workforce and the working class in general.</h4></li></p><br>
+
+<p><li><h4>SSMC is an advocate of the business principle of Corporate Citizenship whereby its business interest is served best with profitability and growth being a
+ccomplished alongside the development of the communities, the protection and sustainability of the environment, and the improvement of people's quality of life.</h4></li></p><br>
+
+
+
