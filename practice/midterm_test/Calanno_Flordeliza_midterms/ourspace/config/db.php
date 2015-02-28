46598b561595cdf7a395edf9f75c630f9f0c2d03
lmcalanno <florcalanno@gmail.com>
2015-02-28 09:12:10 +0800

    modified the db.php

--- practice/midterm_test/Calanno_Flordeliza_midterms/ourspace/config/db.php
+++ practice/midterm_test/Calanno_Flordeliza_midterms/ourspace/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_calanno_flordeliza_ourspace',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
