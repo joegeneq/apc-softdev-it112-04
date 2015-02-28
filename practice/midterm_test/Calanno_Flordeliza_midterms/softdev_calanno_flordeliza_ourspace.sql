f16e091b6a487d06be5b335fa6331f6c66a0015f
lmcalanno <florcalanno@gmail.com>
2015-02-28 09:10:09 +0800

    committed sql file of ourspace

---
+++ practice/midterm_test/Calanno_Flordeliza_midterms/softdev_calanno_flordeliza_ourspace.sql
@@ -0,0 +1,94 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 28, 2015 at 02:06 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `softdev_calanno_flordeliza_ourspace`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `myaddress`
+--
+
+CREATE TABLE IF NOT EXISTS `myaddress` (
+`id` int(11) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `middlename` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `gender` varchar(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mycomment`
+--
+
+CREATE TABLE IF NOT EXISTS `mycomment` (
+`id` int(11) NOT NULL,
+  `myaddress_id` int(11) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `body` longtext NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `myaddress`
+--
+ALTER TABLE `myaddress`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mycomment`
+--
+ALTER TABLE `mycomment`
+ ADD PRIMARY KEY (`id`), ADD KEY `mycomment_ibfk_1` (`myaddress_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `myaddress`
+--
+ALTER TABLE `myaddress`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `mycomment`
+--
+ALTER TABLE `mycomment`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `mycomment`
+--
+ALTER TABLE `mycomment`
+ADD CONSTRAINT `mycomment_ibfk_1` FOREIGN KEY (`myaddress_id`) REFERENCES `myaddress` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
