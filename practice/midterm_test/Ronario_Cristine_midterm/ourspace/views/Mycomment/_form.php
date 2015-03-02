26a0cd257887e4a5ac3862559b31e4204d358a62
clronario <kerstenten@gmail.com>
2015-03-02 14:45:28 -0800

    K. Update field myaddress_id in the form for mycomment to show the last name as a drop down instead of an empty text field.

--- practice/midterm_test/Ronario_Cristine_midterm/ourspace/views/Mycomment/_form.php
+++ practice/midterm_test/Ronario_Cristine_midterm/ourspace/views/Mycomment/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\models\myaddress;
+use yii\helpers\ArrayHelper;
+use yii\widgets\DetailView; 
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Mycomment */
@@ -12,11 +15,15 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id')->textInput() ?>
+    <?php 
+                $myaddress = myaddress::find()->all();
+                $listData=ArrayHelper::map($myaddress,'id', 'lastname');
+                echo $form->field($model, 'myaddress_id')->dropDownList(
+                                                                $listData,
+                                                                ['prompt'=>'Select last name...']) 
+        ?>
 
-    <?= $form->field($model, 'myaddress_id')->textInput() ?>
-
-    <?= $form->field($model, 'author')->textInput(['maxlength' => 225]) ?>
+    <?= $form->field($model, 'author')->textInput(['maxlength' => 255]) ?>
 
     <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
 
