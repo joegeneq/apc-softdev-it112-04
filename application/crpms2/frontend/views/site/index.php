552b9d12b05135bbb7aa87c24f73d059717737e9
clronario <kerstenten@gmail.com>
2015-03-14 09:07:12 +0800

    update index.php and about.php

--- application/crpms2/frontend/views/site/index.php
+++ application/crpms2/frontend/views/site/index.php
@@ -1,12 +1,16 @@
 <?php
 /* @var $this yii\web\View */
+/* @var $form yii\bootstrap\Carousel */
+
 $this->title = 'CRMGH-PMS';
 ?>
-<body background="../images/background5.png">
+<header><center><img src="../images/crpmslogo.png" width="300" height="250" >
+<center><body background="../images/background5.png">
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Welcome to Cruz-Rabe Maternity and General Hospital </h1>
+        <div class="jumbotron">
+		<h1>Welcome to Cruz-Rabe Maternity and General Hospital </h1>
+    </div>
 
 <script type="text/javascript"> 
 
@@ -103,7 +107,7 @@ swapImage();
 
 <td> 
 
-<img name="slide" id="slide" alt ="my images" height="285" width="1000" src="screensho1.jpg"/> 
+<img name="slide" id="slide" alt ="my images" height="285" width="1000" src="screenshot1.jpg"/> 
 
 </td> 
 
@@ -122,51 +126,51 @@ swapImage();
 </table> 
 
 
-
-		
+	
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Mission</h2>
+                <h2>Company Profile</h2>
 
                 <p>
-		SSMC regards all those it serve with the respect,kindness and compassion; <br>
-
-
-		<li>SSMC promotes health and wellness pratices in the worplaces of its client as well as of itself for sound business outcomes;</li>
-		<li>SSMC continiously strives to have a full assortment of suitable equipment and facilities to etter serve its patients;/li>
-		<li>SSMC promotes the development of a consortium of competent and effective health professionals and workers who are recognized 
-			as part of the healthcare family worthhy of being treated with loyalthy, respect and dignity;</li>
-		<li>SSMC networks with labor intensive institutions and acts with outmost, honesty ,integrity and fairness to the best interest of 
-			the working masses;</li>
-		<li>SSMC maintains a sound Quality Assurance Program, continiously strives for improvement and supports its efforts with 
-		certification from various recognized accrediting bodies in Quality, Environment, Safety and health and other areas that may 
-		redeemed relevant and beneficial for all concerned at any given time; and,</li>
-		<li>SSMC networks with benevolent individuals and intstitutions to ensure that resources are made available to effectively 
-		and advocacy programs.</li>
 				
-				</p>
+				<b> With the mission to help the constituents of Taguig in their Medical needs. <b> <br>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+			<li>	1969 - Year of founding, as a 3-bed maternity. <br></li>
+			<li>	1974 - Expanded into a primary general hospital with 12 beds capacity. <br></li>
+			<li>	1981 - Expanded and upgraded into a secondary hospital with 50 beds. <br></li>
+		</p>
+		 <p><a class="btn btn-default" href="http://localhost/crpms2/frontend/web/index.php?r=site%2Fabout">View More &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Vision</h2>
 
                 <p>
-				<li> SSMC is one of the country's leading, trusted and affordable one stop 
-				provider of health care solutions for employers, their workforce and the working class in general.</li>
-				<li> SSMC is an advocate of the business principle of Corporate Citizenship whereby its business 
-				interest is served best with profitability and growth being a ccomplished alongside the development of the communities,
-				the protection and sustainability of the environment, and the improvement of people's quality of life. </li>
+				
+				<li> SSMC is one of the country's leading, trusted and affordable one stop provider of health care solutions for employers, 
+			their workforce and the working class in general.</li> <br>
+				<li>SSMC is an advocate of the business principle of Corporate Citizenship whereby its business interest is served 
+					best with profitability and growth being a ccomplished alongside the development of the communities, the protection 
+					and sustainability of the environment, and the improvement of people's quality of life. </li> <br>
+				</p>
+	 <p><a class="btn btn-default" href="http://localhost/crpms2/frontend/web/index.php?r=site%2Fabout">View More &raquo;</a></p>
+ 
+            </div>
+            <div class="col-lg-4">
+                <h2>Mission</h2>
+
+                <p>
+				
+		<li> SSMC regards all those it serve with the respect,kindness and compassion;</li><br>
+		<li>SSMC promotes health and wellness pratices in the worplaces of its client as well as of itself for sound business outcomes;</li><br>
+		<li>SSMC continiously strives to have a full assortment of suitable equipment and facilities to etter serve its patients;</li><br>
+	 <p><a class="btn btn-default" href="http://localhost/crpms2/frontend/web/index.php?r=site%2Fabout">View More &raquo;</a></p>
+
 				
 				</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
-            
         </div>
 
     </div>
 </div>
-
