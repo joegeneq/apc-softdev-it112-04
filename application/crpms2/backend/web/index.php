312ef22880cb3ec87ddd738d70ef2c04d3ed7b7d
markkevinnavarro <markkevinnavarro@gmail.com>
2015-03-23 22:42:10 -0800

    fixed the error in my backend/web

--- application/crpms2/backend/web/index.php
+++ application/crpms2/backend/web/index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
@@ -17,23 +16,3 @@ $config = yii\helpers\ArrayHelper::merge(
 
 $application = new yii\web\Application($config);
 $application->run();
-=======
-<?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
-require(__DIR__ . '/../../vendor/autoload.php');
-require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
-require(__DIR__ . '/../../common/config/bootstrap.php');
-require(__DIR__ . '/../config/bootstrap.php');
-
-$config = yii\helpers\ArrayHelper::merge(
-    require(__DIR__ . '/../../common/config/main.php'),
-    require(__DIR__ . '/../../common/config/main-local.php'),
-    require(__DIR__ . '/../config/main.php'),
-    require(__DIR__ . '/../config/main-local.php')
-);
-
-$application = new yii\web\Application($config);
-$application->run();
->>>>>>> 0e8049b9449bab1774088fccc8163a14329809ca
