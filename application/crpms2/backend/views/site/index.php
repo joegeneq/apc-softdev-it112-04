679cc68a9c3b97fa2b4b4259d6545276f5b897e3
markkevinnavarro <markkevinnavarro@gmail.com>
2015-03-23 22:54:29 -0800

    Modified the site index.php and site/index.php because it has a <<<<< HEAD >>>> statement inside which is causing my copy of the project to commit errors.

--- application/crpms2/backend/views/site/index.php
+++ application/crpms2/backend/views/site/index.php
@@ -4,20 +4,14 @@
 
 $this->title = 'CRMGH-PMS';
 ?>
-<<<<<<< HEAD
-<header><center><img src="../images/crpmslogo.png" width="300" height="250" >
-<center><body background="../images/background5.png">
-<div class="site-index">
-
-        <div class="jumbotron">
+        <!---div class="jumbotron">
 		<h1>Welcome to Cruz-Rabe Maternity and General Hospital </h1>
-    </div>
+    </div---->
 
-=======
 <header><img src="../images/header1.png" width="1100" height="250" >
 <center><body background="../images/background5.png">
 <div class="site-index">
->>>>>>> 0e8049b9449bab1774088fccc8163a14329809ca
+
 <script type="text/javascript"> 
 
 var i = 0; 
