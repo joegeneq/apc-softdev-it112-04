596608e258f65944ea1d8b75f617dc111906bc91
clronario <kerstenten@gmail.com>
2015-03-25 14:45:11 +0800

    edit main-local.php naming the database

--- application/crpms2/common/config/main-local.php
+++ application/crpms2/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=crpms_softdev',
+            'dsn' => 'mysql:host=localhost;dbname=crpms_softdev2',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
