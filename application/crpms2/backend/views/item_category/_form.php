a82457768978859fe82972190fbed84278fdc858
clronario <kerstenten@gmail.com>
2015-04-01 08:31:06 +0800

    ItemCategory backend/views change to item_category

--- application/crpms2/backend/views/item_category/_form.php
+++ application/crpms2/backend/views/item_category/_form.php
@@ -1,25 +1,20 @@
-
 <?php
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use backend\models\ItemCategory;
-use yii\helpers\ArrayHelper;
+
 /* @var $this yii\web\View */
 /* @var $model backend\models\ItemCategory */
 /* @var $form yii\widgets\ActiveForm */
 ?>
-<body background="../images/background5.png">
+
 <div class="item-category-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
- <?php
-        $itemcategory=ItemCategory::find()->all();
-        $listData=ArrayHelper::map($itemcategory, 'id', 'category_name');
-        echo $form->field($model, 'category_name')->dropDownList(
-            $listData,['prompt'=>'Select Category Name']);
-    ?>
+    <?= $form->field($model, 'category_id')->textInput() ?>
+
+    <?= $form->field($model, 'category_name')->textInput(['maxlength' => 20]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
