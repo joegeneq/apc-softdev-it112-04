23d471ade0c2e6b89ec8d3afe0f0f31eb32bb814
markkevinnavarro <markkevinnavarro@gmail.com>
2015-04-01 13:50:04 +0800

    Inserted dropDownList to every foreign key of the table return-item-details...

--- application/crpms2/frontend/views/return-item-details/_form.php
+++ application/crpms2/frontend/views/return-item-details/_form.php
@@ -2,6 +2,11 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use backend\models\Location;
+use backend\models\AccountingStatus;
+use backend\models\Employee;
+use backend\models\ReturnItemHeader;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\ReturnItemDetails */
@@ -14,11 +19,23 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'return_item_details_code')->textInput(['maxlength' => 20]) ?>
 
-    <?= $form->field($model, 'location_id')->textInput() ?>
+    <!----?= $form->field($model, 'location_id')->textInput() ?---->
+    <?php
+        $location=Location::find()->all();
+        $listData=ArrayHelper::map($location, 'id', 'location_name');
+        echo $form->field($model, 'location_id')->dropDownList(
+            $listData,['prompt'=>'Select Location']);
+    ?>
 
     <?= $form->field($model, 'return_item_header_id')->textInput() ?>
 
-    <?= $form->field($model, 'accounting_status_id')->textInput() ?>
+    <!----?= $form->field($model, 'accounting_status_id')->textInput() ?---->
+    <?php
+        $accountingstatus=AccountingStatus::find()->all();
+        $listData=ArrayHelper::map($accountingstatus, 'id', 'description');
+        echo $form->field($model, 'accounting_status_id')->dropDownList(
+            $listData,['prompt'=>'Select Status']);
+    ?>
 
     <?= $form->field($model, 'employee_id')->textInput() ?>
 
