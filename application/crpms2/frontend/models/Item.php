d7925f2fbedc07a439b9424af4ab490b0bf17444
lmcalanno <florcalanno@gmail.com>
2015-04-01 08:22:01 +0800

    create crud of return_item_header table in frontend

---
+++ application/crpms2/frontend/models/Item.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "item".
+ *
+ * @property integer $id
+ * @property string $item_code
+ * @property string $item_name
+ * @property string $description_item
+ * @property integer $item_category_id
+ *
+ * @property ItemCategory $itemCategory
+ * @property ReturnItemHeader[] $returnItemHeaders
+ * @property StockInventory[] $stockInventories
+ * @property StockIssueHeader[] $stockIssueHeaders
+ */
+class Item extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'item';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['item_code', 'item_name', 'description_item', 'item_category_id'], 'required'],
+            [['description_item'], 'string'],
+            [['item_category_id'], 'integer'],
+            [['item_code'], 'string', 'max' => 5],
+            [['item_name'], 'string', 'max' => 20]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'item_code' => 'Item Code',
+            'item_name' => 'Item Name',
+            'description_item' => 'Description Item',
+            'item_category_id' => 'Item Category ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getItemCategory()
+    {
+        return $this->hasOne(ItemCategory::className(), ['id' => 'item_category_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getReturnItemHeaders()
+    {
+        return $this->hasMany(ReturnItemHeader::className(), ['item_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getStockInventories()
+    {
+        return $this->hasMany(StockInventory::className(), ['item_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getStockIssueHeaders()
+    {
+        return $this->hasMany(StockIssueHeader::className(), ['item_id' => 'id']);
+    }
+}
---
+++ application/crpms2/frontend/models/Location.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "location".
+ *
+ * @property integer $id
+ * @property string $location_code
+ * @property string $location_name
+ *
+ * @property ReturnItemDetails[] $returnItemDetails
+ * @property ReturnItemHeader[] $returnItemHeaders
+ * @property StockInventory[] $stockInventories
+ * @property StockIssueDetails[] $stockIssueDetails
+ */
+class Location extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'location';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['location_code', 'location_name'], 'required'],
+            [['location_code', 'location_name'], 'string', 'max' => 20]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'location_code' => 'Location Code',
+            'location_name' => 'Location Name',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getReturnItemDetails()
+    {
+        return $this->hasMany(ReturnItemDetails::className(), ['location_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getReturnItemHeaders()
+    {
+        return $this->hasMany(ReturnItemHeader::className(), ['location_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getStockInventories()
+    {
+        return $this->hasMany(StockInventory::className(), ['location_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getStockIssueDetails()
+    {
+        return $this->hasMany(StockIssueDetails::className(), ['location_id' => 'id']);
+    }
+}
