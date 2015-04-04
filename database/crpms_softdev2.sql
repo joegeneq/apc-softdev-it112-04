ec4ae3f973f3188492f6b9b554892f55f4579417
clronario <kerstenten@gmail.com>
2015-04-05 00:41:21 +0800

    add patient table

--- database/crpms_softdev2.sql
+++ database/crpms_softdev2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2015 at 06:35 PM
+-- Generation Time: Apr 04, 2015 at 06:40 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
