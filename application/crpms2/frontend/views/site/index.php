dd1a646aded734d94e156ff6f709e63bcbbd5e50
lmcalanno <florcalanno@gmail.com>
2015-04-07 20:25:38 +0800

    frontend image path

--- application/crpms2/frontend/views/site/index.php
+++ application/crpms2/frontend/views/site/index.php
@@ -10,8 +10,8 @@ $this->title = 'CRMGH-PMS';
 	
     </div>
 
-<header><center><img src="../images/header1.png" width="1100" height="250" ></center>
-<center><body background="../images/background5.png">
+<header><center><img src="../web/images/header1.png" width="1100" height="250" ></center>
+<center><body background="../web/images/background5.png">
 <div class="site-index">
 
 
@@ -24,17 +24,17 @@ var image = new Array();
 
 // LIST OF IMAGES 
 
-image[0] = "../images/screenshot1.jpg"; 
+image[0] = "../web/images/screenshot1.jpg"; 
 
-image[1] = "../images/screenshot2.jpg"; 
+image[1] = "../web/images/screenshot2.jpg"; 
 
-image[2] = "../images/screenshot3.jpg";    
+image[2] = "../web/images/screenshot3.jpg";    
 
-image[3] = "../images/screenshot4.jpg"; 
+image[3] = "../web/images/screenshot4.jpg"; 
 
-image[4] = "../images/screenshot5.jpg"; 
+image[4] = "../web/images/screenshot5.jpg"; 
 
-image[5] = "../images/screenshot6.jpg"; 
+image[5] = "../web/images/screenshot6.jpg"; 
 
 var k = image.length-1;    
 
