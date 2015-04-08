397ee61c5e0b71297ba925727933676a09e0cf11
markkevinnavarro <markkevinnavarro@gmail.com>
2015-04-08 09:08:03 +0800

    modify backend/views

--- application/crpms2/backend/views/stock-issue-details/view.php
+++ application/crpms2/backend/views/stock-issue-details/view.php
@@ -33,8 +33,9 @@ $this->params['breadcrumbs'][] = $this->title;
             ['attribute' => 'location.location_name', 'label' => 'Location Name:'],
             //['attribute' => 'stockinventory.stock_inventory_id', 'label' => 'Stock inventory id:'],
             'stock_issue_code',
-            ['attribute' => 'stockstatus.description_name', 'label' => 'Stock issued id:'],
-           ['attribute' => 'employee.employee_id', 'label' => 'Employee Name:'],
+            
+            ['attribute' => 'stockStatus.description_name', 'label' => 'Stock Description:'],
+            ['attribute' => 'employee.lastname', 'label' => 'Employee Name:'],
             'issue_from',
         ],
     ]) ?>
