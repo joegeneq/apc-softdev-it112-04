11a62bd6c469eb2e1818bc7eff7f4b3081463c3d
lmcalanno <florcalanno@gmail.com>
2015-04-15 19:34:29 +0800

    debug

--- application/crpms2/backend/views/return-item-header/_form.php
+++ application/crpms2/backend/views/return-item-header/_form.php
@@ -6,7 +6,7 @@ use yii\helpers\ArrayHelper;
 use backend\models\Location;
 use backend\models\Patient;
 use backend\models\Bed;
-use backend\models\ReturnItemHeader;
+
 /* @var $this yii\web\View */
 /* @var $model backend\models\ReturnItemHeader */
 /* @var $form yii\widgets\ActiveForm */
@@ -21,12 +21,10 @@ use backend\models\ReturnItemHeader;
         ['prompt'=>'Select Patient'] ) 
     ?>
 
-   <?php
-        $returnItemHeader=ReturnItemHeader::find()->all();
-        $listData=ArrayHelper::map($returnItemHeader, 'id', 'return_item_header_code');
-        echo $form->field($model, 'return_item_header_code')->dropDownList(
-            $listData,['prompt'=>'Select Return Item Header Code']);
-    ?>
+
+    <?= $form->field($model, 'return_item_header_code')->textInput(['maxlength' => 20]) ?>
+
+  
 	
    <?php
         $location=Location::find()->all();
@@ -46,8 +44,7 @@ use backend\models\ReturnItemHeader;
 
     <?= $form->field($model, 'remarks')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'created')->textInput() ?>
-
+   
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
