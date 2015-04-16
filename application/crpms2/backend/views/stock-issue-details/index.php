4409abbd15ef09bf5c55e5f6af592735e61a945c
clronario <kerstenten@gmail.com>
2015-04-16 20:29:18 +0800

    stock issue details put dropdown on the index.php

--- application/crpms2/backend/views/stock-issue-details/index.php
+++ application/crpms2/backend/views/stock-issue-details/index.php
@@ -27,9 +27,22 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\SerialColumn'],
 
           //  'id',
-            'stock_issue_code',
+      
+			['attribute' => 'stock_issue_code',
+            'label' => 'Stock Issue Code',
+            'value' => 'stockIssueDetails.stock_issue_code',
+            'filter' => yii\helpers\ArrayHelper::map(backend\models\StockIssueDetails::find()-> all(),'id','stock_issue_code')],
+           
+			
+			
             'date_created',
-            'item_id',
+			
+           ['attribute' => 'item_id',
+            'label' => 'Item name',
+            'value' => 'item.item_name',
+            'filter' => yii\helpers\ArrayHelper::map(backend\models\Item::find()-> all(),'id','item_name')],
+         
+		  
             'quantity',
             // 'exp_date',
             // 'unit_cost',
