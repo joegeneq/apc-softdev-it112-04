942bd75907f9041478a6a0e2ee99c4f5d62f9b23
clronario <kerstenten@gmail.com>
2015-04-20 00:25:36 +0800

    dropdown in the Stock Issue details index.php

--- application/crpms2/backend/views/stock-issue-details/index.php
+++ application/crpms2/backend/views/stock-issue-details/index.php
@@ -10,10 +10,7 @@ use yii\grid\GridView;
 $this->title = 'Stock Issue Details';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<<<<<<< HEAD
-=======
 <body background="../web/images/background5.png">
->>>>>>> 0c8455709e04324e7a9573e59786d78064fa4dbe
 <div class="stock-issue-details-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
@@ -30,22 +27,11 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\SerialColumn'],
 
             //'id',
-<<<<<<< HEAD
-              
-             ['attribute' => 'stock_issue_header_id',
-            'label' => 'Stock Issue Header Code',
-            'value' => 'stockIssueHeader.header_code',
-            'filter' => yii\helpers\ArrayHelper::map(backend\models\StockIssueHeader::find()-> all(),'id','header_code')],
-           
+          
             'date_release',
-           ['attribute' => 'item_id',
-            'label' => 'Item Name',
-=======
-        'stock_issue_code',
-            'date_created',
-               ['attribute' => 'item_id',
+
+			  ['attribute' => 'item_id',
             'label' => 'Item name',
->>>>>>> 0c8455709e04324e7a9573e59786d78064fa4dbe
             'value' => 'item.item_name',
             'filter' => yii\helpers\ArrayHelper::map(backend\models\Item::find()-> all(),'id','item_name')],
          
@@ -54,11 +40,14 @@ $this->params['breadcrumbs'][] = $this->title;
             // 'unit_cost',
             // 'amount',
             // 'remarks:ntext',
-<<<<<<< HEAD
             // 'date_created',
-=======
-            // 'stock_issue_header_id',
->>>>>>> 0c8455709e04324e7a9573e59786d78064fa4dbe
+            
+			['attribute' => 'stock_issue_header_id',
+            'label' => 'Stock Issue Header Code',
+            'value' => 'stockIssueHeader.header_code',
+            'filter' => yii\helpers\ArrayHelper::map(backend\models\StockIssueHeader::find()-> all(),'id','header_code')],
+           
+
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
