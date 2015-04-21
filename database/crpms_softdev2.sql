55cf6d57c0027801004a9e9407dd5373918c2e29
lmcalanno <florcalanno@gmail.com>
2015-04-21 13:03:46 +0800

    updated database

--- database/crpms_softdev2.sql
+++ database/crpms_softdev2.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 21, 2015 at 05:56 AM
+-- Generation Time: Apr 21, 2015 at 07:01 AM
 -- Server version: 5.6.21
--- PHP Version: 5.5.19
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -443,14 +443,15 @@ CREATE TABLE IF NOT EXISTS `unit_of_measure` (
 `id` int(11) NOT NULL,
   `type_of_measure` varchar(25) NOT NULL,
   `description` longtext
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `unit_of_measure`
 --
 
 INSERT INTO `unit_of_measure` (`id`, `type_of_measure`, `description`) VALUES
-(1, 'mg', 'Milligrams');
+(1, 'mg', 'Milligrams'),
+(2, 'kg', 'Kilograms');
 
 -- --------------------------------------------------------
 
@@ -500,7 +501,7 @@ ALTER TABLE `bed`
 -- Indexes for table `bed_status`
 --
 ALTER TABLE `bed_status`
- ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`), ADD KEY `id` (`id`);
 
 --
 -- Indexes for table `employee`
@@ -691,7 +692,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 -- AUTO_INCREMENT for table `unit_of_measure`
 --
 ALTER TABLE `unit_of_measure`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
@@ -701,6 +702,12 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 ALTER TABLE `bed`
 ADD CONSTRAINT `bed_ibfk_3` FOREIGN KEY (`location_id`) REFERENCES `location` (`id`);
+
+--
+-- Constraints for table `employee`
+--
+ALTER TABLE `employee`
+ADD CONSTRAINT `employee_ibfk_8` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `item`
@@ -754,7 +761,8 @@ ADD CONSTRAINT `stock_issue_header_ibfk_2` FOREIGN KEY (`stock_inventory_id`) RE
 ADD CONSTRAINT `stock_issue_header_ibfk_3` FOREIGN KEY (`location_id`) REFERENCES `location` (`id`),
 ADD CONSTRAINT `stock_issue_header_ibfk_6` FOREIGN KEY (`location_id`) REFERENCES `location` (`id`),
 ADD CONSTRAINT `stock_issue_header_ibfk_7` FOREIGN KEY (`stock_inventory_id`) REFERENCES `stock_inventory` (`id`),
-ADD CONSTRAINT `stock_issue_header_ibfk_8` FOREIGN KEY (`stock_status_id`) REFERENCES `stock_status` (`id`);
+ADD CONSTRAINT `stock_issue_header_ibfk_8` FOREIGN KEY (`stock_status_id`) REFERENCES `stock_status` (`id`),
+ADD CONSTRAINT `stock_issue_header_ibfk_9` FOREIGN KEY (`created_by`) REFERENCES `user` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
